87dbeffb3de35b9b7ab4730105eb72a165ff96f6
Jochen <jochen.handels@web.de>
2019-02-26 18:26:22 +0100

    Added script to generate favicon

--- Tasks/scripts/generate_icons.js
+++ Tasks/scripts/generate_icons.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const child_process = require('child_process');
 
 const source = path.resolve(__dirname, `../public/logo_512.svg`);
@@ -7,20 +8,23 @@ const sizes = [512,256,192,144,128,96,72,64,48,32,16];
 sizes.forEach(size => {
     const target = path.resolve(__dirname, `../public/icon-${size}-${size}.png`);
 
-    const cmd = `
+    const inkscapeCmd = `
         inkscape -z ${source}
         --export-png=${target}
         --export-height=${size}
         --export-width=${size}
         `.replace(/\n/g, " ").replace(/[ ]+/g, " ").trim();
 
-    child_process.exec(cmd, (err, stdout, stderr) => {
-        const log = function (line, stream) {
-            if (line.length > 0) {
-                stream(line);
-            }
-        };
-        log(stdout, console.log.bind(console));
-        log(stderr, console.error.bind(console));
-    });
+    child_process.execSync(inkscapeCmd);
 });
+
+var gimpScript = fs.readFileSync(
+    path.resolve(__dirname, "favicon.scm"),
+    "utf8");
+const gimpCmd = "gimp-console-2.10 -i -b -";
+const gimpCmdOptions = {
+    cwd: path.resolve(__dirname, ".."),
+    input: gimpScript
+};
+
+child_process.execSync(gimpCmd, gimpCmdOptions);
