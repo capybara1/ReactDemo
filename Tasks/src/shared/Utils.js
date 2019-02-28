ccfd958c7a3cf1e27cfd5f9377f62e8022526788
Jochen <jochen.handels@web.de>
2019-02-28 20:22:46 +0100

    Introduced static type checking on Utils.js

--- Tasks/src/shared/Utils.js
+++ Tasks/src/shared/Utils.js
@@ -1,6 +1,8 @@
+//@flow
+
 export const TODAY = new Date().toISOString().slice(0, 10);
 
-export function generateId() {
+export function generateId(): string {
   const result = (((1 + Math.random()) * 0x10000) | 0)
     .toString(16)
     .substring(1);
