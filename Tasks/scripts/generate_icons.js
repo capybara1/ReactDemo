5591e0eedff2ebfb955bc59dc0e77dc7b89fbaf6
Jochen <jochen.handels@web.de>
2019-02-28 19:09:48 +0100

    Ran prettier on generate_incons.js

--- Tasks/scripts/generate_icons.js
+++ Tasks/scripts/generate_icons.js
@@ -1,30 +1,34 @@
-const path = require('path');
-const fs = require('fs');
-const child_process = require('child_process');
+const path = require("path");
+const fs = require("fs");
+const child_process = require("child_process");
 
 const source = path.resolve(__dirname, `../public/logo_512.svg`);
-const sizes = [512,192,144,96,72,48];
+const sizes = [512, 192, 144, 96, 72, 48];
 
 sizes.forEach(size => {
-    const target = path.resolve(__dirname, `../public/icon-${size}-${size}.png`);
+  const target = path.resolve(__dirname, `../public/icon-${size}-${size}.png`);
 
-    const inkscapeCmd = `
+  const inkscapeCmd = `
         inkscape -z ${source}
         --export-png=${target}
         --export-height=${size}
         --export-width=${size}
-        `.replace(/\n/g, " ").replace(/[ ]+/g, " ").trim();
+        `
+    .replace(/\n/g, " ")
+    .replace(/[ ]+/g, " ")
+    .trim();
 
-    child_process.execSync(inkscapeCmd);
+  child_process.execSync(inkscapeCmd);
 });
 
 var gimpScript = fs.readFileSync(
-    path.resolve(__dirname, "favicon.scm"),
-    "utf8");
+  path.resolve(__dirname, "favicon.scm"),
+  "utf8"
+);
 const gimpCmd = "gimp-console-2.10 -i -b -";
 const gimpCmdOptions = {
-    cwd: path.resolve(__dirname, ".."),
-    input: gimpScript
+  cwd: path.resolve(__dirname, ".."),
+  input: gimpScript
 };
 
 child_process.execSync(gimpCmd, gimpCmdOptions);
