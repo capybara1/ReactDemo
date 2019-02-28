beb3b1a710f1c98d3747c6c9037ac408dfc7261c
Jochen <jochen.handels@web.de>
2019-02-28 19:12:41 +0100

    Ran prettier on config overrides

--- Tasks/config-overrides.js
+++ Tasks/config-overrides.js
@@ -1,44 +1,46 @@
-const { InjectManifest } = require('workbox-webpack-plugin');
-const WebpackShellPlugin = require('webpack-shell-plugin');
-const path = require('path');
+const { InjectManifest } = require("workbox-webpack-plugin");
+const WebpackShellPlugin = require("webpack-shell-plugin");
+const path = require("path");
 
 module.exports = {
-    webpack: function(config, env) {
-        if (env === 'production') {
-            config = removePlugin(config, 'GenerateSW');
-            const workboxConfigProd = {
-              swSrc: path.join(__dirname, 'public', 'service-worker.js'),
-              swDest: 'service-worker.js',
-              importWorkboxFrom: 'local'
-            };
-            config.plugins.push(new InjectManifest(workboxConfigProd));
-        }
+  webpack: function(config, env) {
+    if (env === "production") {
+      config = removePlugin(config, "GenerateSW");
+      const workboxConfigProd = {
+        swSrc: path.join(__dirname, "public", "service-worker.js"),
+        swDest: "service-worker.js",
+        importWorkboxFrom: "local"
+      };
+      config.plugins.push(new InjectManifest(workboxConfigProd));
+    }
 
-        config.plugins.push(new WebpackShellPlugin({
-            onBuildStart: ['node "scripts/generate_icons.js"'],
-        }));
+    config.plugins.push(
+      new WebpackShellPlugin({
+        onBuildStart: ['node "scripts/generate_icons.js"']
+      })
+    );
 
-        return config;
-    }, 
-    // jest: function(config) {
-    //     return config;
-    // },
-    // devServer: function(configFunction) {
-    //     console.log(configFunction.toString());
-    //     return function (proxy, allowedHost) {
-    //         console.log(proxy, allowedHost);
-    //         const config = configFunction(proxy, allowedHost);
-    //         return config;
-    //     }
-    // }
-}
+    return config;
+  }
+  // jest: function(config) {
+  //     return config;
+  // },
+  // devServer: function(configFunction) {
+  //     console.log(configFunction.toString());
+  //     return function (proxy, allowedHost) {
+  //         console.log(proxy, allowedHost);
+  //         const config = configFunction(proxy, allowedHost);
+  //         return config;
+  //     }
+  // }
+};
 
 function removePlugin(config, name) {
-    const pluginIndex = config.plugins.findIndex((element) => {
-        return element.constructor.name === name;
-    })
-    if (pluginIndex !== -1) {
-        config.plugins.splice(pluginIndex, 1);
-    }
-    return config;
+  const pluginIndex = config.plugins.findIndex(element => {
+    return element.constructor.name === name;
+  });
+  if (pluginIndex !== -1) {
+    config.plugins.splice(pluginIndex, 1);
+  }
+  return config;
 }
