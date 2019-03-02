2b8faf75f728c1f18bf0ae1f5f3c8cf879bb2260
Jochen <jochen.handels@web.de>
2019-03-02 19:15:46 +0100

    Improved layout for about page

--- Tasks/src/components/About.js
+++ Tasks/src/components/About.js
@@ -1,7 +1,7 @@
 import React from "react"; // Required for JSX
 
 const About = () => (
-  <div>
+  <div className="container">
     <h2>About Tasks</h2>
     <p>A demo app for react and related technologies</p>
   </div>
